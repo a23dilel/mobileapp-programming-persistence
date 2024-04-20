25840754bacee36f0ba9fa9750c540ed3eb7dfc6
a23dilel <a23dilel@student.his.se>
2024-04-20 18:45:41 +0200

    Implemented methods in DatabaseHelper.java

--- app/src/main/java/com/example/persistence/DatabaseHelper.java
+++ app/src/main/java/com/example/persistence/DatabaseHelper.java
@@ -1,4 +1,23 @@
 package com.example.persistence;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
