b1f37097db22713b2ad7f0365102b62d95b898f0
a23dilel <a23dilel@student.his.se>
2024-04-21 22:25:02 +0200

    Simplify in DatabaseTables.java for removing the method and properties.

--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -1,52 +1,22 @@
 package com.example.persistence;
 
 public class DatabaseTables {
-    public static String TABLE_NAME = "Hello";
-    public static String[] COLUMN_VALUES;
-
+    public static String TABLE_NAME = "DATA_TABLE";
     public static final String COLUMN_ID = "ID";
-    public static final String COLUMN_NAME = "Name";
-    public static final String COLUMN_VALUE = "Value";
+    public static final String COLUMN_NAME = "NAME";
+    public static final String COLUMN_LOCATION = "LOCATION";
 
-    /*
-    public DatabaseTables(String tableName, String[] totalValues)
-    {
-        TABLE_NAME = tableName;
-        COLUMN_VALUES = new String[totalValues.length];
-    }
-
+    /* create a table like this:
+        TABLE
+        |   ID  |   NAME    |   LOCATION    |
      */
 
-    static final String SQL_CREATE_TABLE_DATA = Table();
+    static final String SQL_CREATE_TABLE_DATA = "CREATE TABLE " + TABLE_NAME +
+                    " (" + COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                    COLUMN_NAME + " TEXT, " +
+                    COLUMN_LOCATION + " TEXT " +
+                    ")";
 
-    // "DROP TABLE IF EXISTS mountain"
+    // Remove/drop the table
     static final String SQL_DELETE_TABLE_DATA = "DROP TABLE IF EXISTS " + TABLE_NAME;
-
-    private static String Table()
-    {
-        String table =
-                "CREATE TABLE " + TABLE_NAME +
-                " (" + COLUMN_ID + " INTEGER PRIMARY KEY, " +
-                        COLUMN_NAME + " TEXT ";
-        /*
-        if (COLUMN_VALUES != null)
-        {
-            for (int i = 0; i < COLUMN_VALUES.length; i++)
-            {
-                table += COLUMN_VALUE + " TEXT, ";
-            }
-        }
-        else
-        {
-            table += "NULL" + " TEXT, ";
-        }
-
-         */
-
-        //table = table.substring(0, table.length() - 2);
-
-        table += ");";
-
-        return table;
-    }
 }
