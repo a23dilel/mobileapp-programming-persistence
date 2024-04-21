b9f7da7c8767cc2d2c483cb9c89eb7c4ecedc2c5
a23dilel <a23dilel@student.his.se>
2024-04-21 22:20:03 +0200

    Removed the method and changed data object to string in Data.java

--- app/src/main/java/com/example/persistence/Data.java
+++ app/src/main/java/com/example/persistence/Data.java
@@ -1,41 +1,15 @@
 package com.example.persistence;
 
-import java.util.Arrays;
-
 @SuppressWarnings("WeakerAccess")
 public class Data {
     private String id;
     private String name;
-    private Object[] values;
+    private String location;
 
-    // checking each index and turn into string if index is integer
-    private void ConvertIntegersToStrings(Object[] myValues)
-    {
-        // checking each index
-        for (int i = 0; i < myValues.length; i++)
-        {
-            // store one index in a value
-            Object value = myValues[i];
-
-            // if index has a integer then turn into a string and initialize
-            if (value instanceof Integer)
-            {
-                this.values[i] = String.valueOf(value);
-            }
-            else
-            {
-                this.values[i] = value;
-            }
-        }
-    }
-    public Data(String id, String name, Object ... values) {
+    public Data(String id, String name, String location) {
         this.id = id;
         this.name = name;
-
-        // calculate how many index and initialize
-        this.values = new Object[values.length];
-
-        ConvertIntegersToStrings(values);
+        this.location = location;
     }
 
     public String getID() {
@@ -44,35 +18,26 @@ public class Data {
     public String getName() {
         return this.name;
     }
-    public String[] getValues() {
-
-        String[] stringValues = new String[this.values.length];
-
-        for (int i = 0; i < this.values.length; i++)
-        {
-            stringValues[i] = String.valueOf(values[i]);
-        }
-
-        return stringValues;
+    public String getLocation() {
+        return this.location;
     }
+
     public void setID(String id) {
         this.id = id;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-    public void setValues(Object ... values) {
-
-        ConvertIntegersToStrings(values);
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     @Override
     public String toString() {
-        return "Mountain{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                ", values=" + Arrays.toString(values) +
+        return "Data{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
                 '}';
     }
 }
