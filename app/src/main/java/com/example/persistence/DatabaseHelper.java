b4a30b783711d700295e70dc6f2190d66357418e
a23dilel <a23dilel@student.his.se>
2024-04-21 18:07:00 +0200

    Implemented the addData method to insert data into a database table in DatabaseHelper.java

--- app/src/main/java/com/example/persistence/DatabaseHelper.java
+++ app/src/main/java/com/example/persistence/DatabaseHelper.java
@@ -1,30 +1,52 @@
 package com.example.persistence;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1; // If this is incremented onUpgrade() will be executed
-    private static final String DATABASE_NAME = "Mountain.db"; // The file name of our database
 
-    DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    private DatabaseTables databaseTables;
+
+    DatabaseHelper(Context context, String databaseFileName) {
+        super(context, databaseFileName + ".db", null, DATABASE_VERSION);
     }
 
     // This method is executed only if there is not already a database in the file `Mountain.db`
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(DatabaseTables.SQL_CREATE_TABLE_MOUNTAIN);
+        sqLiteDatabase.execSQL(DatabaseTables.SQL_CREATE_TABLE_DATA);
     }
 
     // This method is executed only if the database version has changed, e.g. from 1 to 2
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        sqLiteDatabase.execSQL(DatabaseTables.SQL_DELETE_TABLE_MOUNTAIN);
+        sqLiteDatabase.execSQL(DatabaseTables.SQL_DELETE_TABLE_DATA);
         onCreate(sqLiteDatabase);
+    }
+
+    public boolean addData(Data data)
+    {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(DatabaseTables.COLUMN_ID, data.getID());
+        cv.put(DatabaseTables.COLUMN_NAME, data.getName());
+
+
+        long insert = database.insert(DatabaseTables.TABLE_NAME, null, cv);
+
+        if (insert != -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 }
